47c8db7fd5c7c64aec611cf98b676cc722a007e4
stavreskimario <59723836+stavreskimario@users.noreply.github.com>
2021-09-24 13:08:09 +1000

    Create main.js

---
+++ Lab 4.4/main.js
@@ -0,0 +1,23 @@
+  // defining width and height
+  var w = 500;
+  var h = 300;
+
+  var projection = d3
+      .geoMercator()
+      .center([145, -36.5])
+      .translate([w / 2, h / 2])
+      .scale(2450);
+
+  var path = d3.geoPath().projection(projection);
+
+  //create svg element
+  var svg = d3
+      .select("body")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h)
+      .attr("fill", "grey");
+
+  d3.json("LGA_VIC.json").then(function(json) {
+      svg.selectAll("path").data(json.features).enter().append("path").attr("d", path);
+  });
